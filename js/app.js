e64d8fa95e4f74acb416e523190c01db9eed02a4
HalaM5 <hala_mm_5@yahoo.com>
2022-11-26 04:49:21 +0200

    used javascript to add class that is targeted by css for swap between sign-up and sign-in

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const signUp =document.getElementById("signUp");
+signUp.addEventListener("click", ()=>{
+    document.getElementById("container").classList.add("swip")})
+
+    const signIn = document.getElementById("signIn");
+    signIn.addEventListener("click", ()=>{
+    document.getElementById("container").classList.remove("swip")})
